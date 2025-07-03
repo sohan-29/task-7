abdb0e2a93d2e3223b6b25bfb45215b56f13bdec
Sohan <sohan291106@gmail.com>
2025-07-03 12:59:02 +0530

    created dynamic elements to show userdata on ui

--- script.js
+++ script.js
@@ -3,7 +3,28 @@ const fetchData = async () => {
     .then(async (response) => {
       const data = await response.json();
       console.log(data);
-      return data;
+      var container = document.getElementsByClassName('mainContainer')[0];
+      data.forEach(user => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        const userName = document.createElement('h3');
+        userName.textContent = user.name;
+        userName.className = 'name';
+        const id = document.createElement('p');
+        id.textContent = user.id;
+        id.className = 'id';
+        const userEmail = document.createElement('p');
+        userEmail.textContent = user.email;
+        userEmail.className = 'email';
+        const userAddress = document.createElement('p');
+        const address = [user.address?.street, user.address?.city, user.address?.zipcode]
+        userAddress.textContent = address.join(', ');
+        userAddress.className = 'address';
+        container.appendChild(id);
+        container.appendChild(userName);
+        container.appendChild(userEmail);
+        container.appendChild(userAddress);
+      });
     })
     .catch(error => {
       console.error('Error:', error);

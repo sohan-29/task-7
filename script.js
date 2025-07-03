518972ecd7481284b3fa3ca6e9579aad6dce57d7
Sohan <sohan291106@gmail.com>
2025-07-03 13:50:56 +0530

    modified card structure and applied styles

--- script.js
+++ script.js
@@ -7,19 +7,39 @@ const fetchData = async () => {
       data.forEach(user => {
         const card = document.createElement('div');
         card.className = 'card';
-        const userName = document.createElement('h3');
-        userName.textContent = user.name;
+
+        const userName = document.createElement('p');
+        const name = document.createElement('span');
+        const nameText = document.createTextNode(user.name);
+        name.className = 'label';
+        name.textContent = 'Name: ';
         userName.className = 'name';
+        userName.appendChild(name);
+        userName.appendChild(nameText)
+
         const id = document.createElement('p');
-        id.textContent = user.id;
+        id.textContent = '#' + user.id;
         id.className = 'id';
+
         const userEmail = document.createElement('p');
-        userEmail.textContent = user.email;
+        const email = document.createElement('span');
+        const emailText = document.createTextNode(user.email);
+        email.className = 'label';
+        email.textContent = 'Email: ';
         userEmail.className = 'email';
+        userEmail.appendChild(email);
+        userEmail.appendChild(emailText);
+
         const userAddress = document.createElement('p');
-        const address = [user.address?.street, user.address?.city, user.address?.zipcode]
-        userAddress.textContent = address.join(', ');
+        const fullAddress = [user.address?.street, user.address?.city, user.address?.zipcode + '.']
+        const address = document.createElement('span');
+        const addressText = document.createTextNode(fullAddress.join(', '));
+        address.className = 'label';
+        address.textContent = 'Address: ';
         userAddress.className = 'address';
+        userAddress.appendChild(address);
+        userAddress.appendChild(addressText)
+        
         card.appendChild(id);
         card.appendChild(userName);
         card.appendChild(userEmail);

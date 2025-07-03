7398ebd8a7d7402b8d5d65baebeb61b97096a1d1
Sohan <sohan291106@gmail.com>
2025-07-03 12:44:00 +0530

    modified fetch

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const fetchData = async() => {
-    const data = await fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json() )
-    .then(data => console.log(data))
+const fetchData = async () => {
+  await fetch('https://jsonplaceholder.typicode.com/users')
+    .then(async (response) => {
+      const data = await response.json();
+      console.log(data);
+      return data;
+    })
     .catch(error => {
-        console.error('Error:', error);
+      console.error('Error:', error);
+      return error;
     })
 }
 
